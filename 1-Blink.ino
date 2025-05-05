ebc1e04b487792bfa557f112729dee060eadf9c8
msraynsford <m_raynsford@hotmail.com>
2025-05-05 23:30:05 +0100

    Create 1-Blink.ino

---
+++ 1-Blink.ino
@@ -0,0 +1,26 @@
+#include <FastLED.h>
+
+#define INTERVAL  1000
+
+const uint8_t ledPin = LED_BUILTIN;
+
+bool ledState = false;
+
+uint8_t brightness = 20;   // Set the led for half brightness.
+
+void setup() {
+  pinMode(ledPin, OUTPUT);      // Initialise the LED pin as an output
+}
+
+void loop() {
+  // Every second turn the LED On/Off
+  EVERY_N_MILLISECONDS(INTERVAL) {        
+    ledState = !ledState;                 // Toggle the state of the LED
+    
+    if(ledState) {
+      analogWrite(ledPin, 255 - brightness);   // Turn the led on
+    } else {
+      analogWrite(ledPin, 255);                // Turn the led off
+    }
+  }
+}
